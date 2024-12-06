5198c97e4bf2c5ce5743343805e82da62725f613
ifine2002 <boyyhp1@gmail.com>
2024-12-06 23:02:35 +0700

    update user

--- src/main/java/vn/ifine/jobhunter/controller/UserController.java
+++ src/main/java/vn/ifine/jobhunter/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,5 +41,11 @@ public class UserController {
     @GetMapping("/user")
     public List<User> getAllUser() {
         return this.userService.fetchAllUser();
+    }
+
+    @PutMapping("/user")
+    public User updateUser(@RequestBody User user) {
+        User currentUser = this.userService.handleUpdateUser(user);
+        return currentUser;
     }
 }
