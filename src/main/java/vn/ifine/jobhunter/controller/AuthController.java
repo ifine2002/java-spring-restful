25d8d3232698cdc179522ebd1f3e98d39ab72fcb
ifine2002 <boyyhp1@gmail.com>
2024-12-09 15:04:09 +0700

    Create response login api

--- src/main/java/vn/ifine/jobhunter/controller/AuthController.java
+++ src/main/java/vn/ifine/jobhunter/controller/AuthController.java
@@ -11,8 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
+import vn.ifine.jobhunter.domain.User;
 import vn.ifine.jobhunter.domain.dto.LoginDTO;
 import vn.ifine.jobhunter.domain.dto.ResLoginDTO;
+import vn.ifine.jobhunter.service.UserService;
 import vn.ifine.jobhunter.util.SecurityUtil;
 
 @RestController
@@ -21,11 +23,13 @@ public class AuthController {
 
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
     private final SecurityUtil securityUtil;
+    private final UserService userService;
 
     public AuthController(AuthenticationManagerBuilder authenticationManagerBuilder,
-            SecurityUtil securityUtil) {
+            SecurityUtil securityUtil, UserService userService) {
         this.authenticationManagerBuilder = authenticationManagerBuilder;
         this.securityUtil = securityUtil;
+        this.userService = userService;
     }
 
     @PostMapping("/login")
@@ -41,6 +45,12 @@ public class AuthController {
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
         ResLoginDTO res = new ResLoginDTO();
+        User currentUserDB = this.userService.handleUserByUsername(loginDTO.getUsername());
+        if (currentUserDB != null) {
+            ResLoginDTO.UserLogin userLogin = new ResLoginDTO.UserLogin(currentUserDB.getId(), currentUserDB.getEmail(),
+                    currentUserDB.getName());
+            res.setUser(userLogin);
+        }
         res.setAccessToken(access_token);
         return ResponseEntity.ok().body(res);
     }
