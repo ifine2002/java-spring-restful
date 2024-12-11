f6dcc86586b6eb4f10ff03184e266f8965d6504f
ifine2002 <boyyhp1@gmail.com>
2024-12-11 18:02:58 +0700

    Config read file local(on server)

--- src/main/java/vn/ifine/jobhunter/config/SecurityConfiguration.java
+++ src/main/java/vn/ifine/jobhunter/config/SecurityConfiguration.java
@@ -46,7 +46,8 @@ public class SecurityConfiguration {
                 .cors(Customizer.withDefaults())
                 .csrf(c -> c.disable())
                 .authorizeHttpRequests(
-                        authorize -> authorize.requestMatchers("/", "/api/v1/auth/login", "/api/v1/auth/refresh")
+                        authorize -> authorize
+                                .requestMatchers("/", "/api/v1/auth/login", "/api/v1/auth/refresh", "/storage/**")
                                 .permitAll()
                                 .anyRequest().authenticated())
                 .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults())
