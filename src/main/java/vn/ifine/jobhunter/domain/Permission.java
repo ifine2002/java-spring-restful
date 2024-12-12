2bf6c822b1d990a6af9d12362e0f5a778a6df84c
ifine2002 <boyyhp1@gmail.com>
2024-12-13 00:35:15 +0700

    Add Model Role, Permission

---
+++ src/main/java/vn/ifine/jobhunter/domain/Permission.java
@@ -0,0 +1,70 @@
+package vn.ifine.jobhunter.domain;
+
+import java.time.Instant;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+import vn.ifine.jobhunter.util.SecurityUtil;
+
+@Entity
+@Table(name = "permissions")
+@Setter
+@Getter
+public class Permission {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotBlank(message = "name không được để trống")
+    private String name;
+
+    @NotBlank(message = "apiPath không được để trống")
+    private String apiPath;
+
+    @NotBlank(message = "method không được để trống")
+    private String method;
+
+    @NotBlank(message = "module không được để trống")
+    private String module;
+
+    private Instant createdAt;
+    private Instant updatedAt;
+
+    private String createdBy;
+    private String updatedBy;
+
+    @ManyToMany(mappedBy = "permissions", fetch = FetchType.LAZY)
+    @JsonIgnore
+    private List<Role> roles;
+
+    @PrePersist
+    public void handleBeforeCreate() {
+        this.createdAt = Instant.now();
+        this.createdBy = SecurityUtil.getCurrentUserLogin().isPresent() == true
+                ? SecurityUtil.getCurrentUserLogin().get()
+                : "";
+    }
+
+    @PreUpdate
+    public void handleBeforeUpdate() {
+        this.updatedAt = Instant.now();
+        ;
+        this.updatedBy = SecurityUtil.getCurrentUserLogin().isPresent() == true
+                ? SecurityUtil.getCurrentUserLogin().get()
+                : "";
+    }
+}
